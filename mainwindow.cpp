9131a966763ab94ba193eeacb8c9b618c6694d18
lampvideo <alvk.klimov@gmail.com>
2019-04-26 09:50:45 +0300

    fix paths

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,9 +7,12 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     videoWidget = new QVideoWidget;
+    player = new QMediaPlayer();
     player->setVideoOutput(videoWidget);
+    
     videoWidget->show();
-
+    QString str="/home/box/dmb1.mp4";
+    player->setMedia(QMediaContent(QUrl::fromLocalFile(str)));
     player->play();
 }
 
